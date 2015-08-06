65a9d35daeaa81eae87265073388e3b61431be99
Quan Do <quandm@smartosc.com>
2015-08-06 13:59:33 +0700

    â

--- resources/views/frontend/box_comment.blade.php
+++ resources/views/frontend/box_comment.blade.php
@@ -1,3 +1,3 @@
 <div class="comment-post">
-    <div class="fb-comments" data-href="http://developers.facebook.com/docs/plugins/comments/" data-numposts="5"></div>
+    <div class="fb-comments" data-href="https://www.facebook.com/www.giaocolam.vn" data-width="500" data-numposts="5"></div>
 </div>
