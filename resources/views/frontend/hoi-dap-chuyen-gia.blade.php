e4b7c89837945039f7c5468eb27b15e3784ac546
Quan Do <quandm@smartosc.com>
2015-08-07 15:29:26 +0700

    a

--- resources/views/frontend/hoi-dap-chuyen-gia.blade.php
+++ resources/views/frontend/hoi-dap-chuyen-gia.blade.php
@@ -32,21 +32,7 @@
                             <div class="clear"></div>
                     </div>
                 </div>
-                <div class="box-contact">
-                    <div class="bg-title03">
-                        <span>Đặt câu hỏi với chuyên gia</span>
-                    </div>
-                    <div class="col-right">
-                        {!! Form::open(['method' => 'POST', 'route' => ['createQuestion'], 'name' => 'questionForm']) !!}
-                        <input type="text" name="ask_person" class="txt txt-name" placeholder="Họ và tên"/>
-                        <input type="email" name="ask_email" class="txt txt-email" placeholder="Email"/>
-                        <input type="number" name="ask_phone" class="txt txt-phone" placeholder="Số điện thoại"/>
-                        <textarea name="question" class="txt txt-content" placeholder="Nội dung"></textarea>
-                        <input type="submit" value="gửi đi" class="btn btn-submit"/>
-                        <span class="mail-name">Chamsockhachhang@gmail.com</span>
-                        {!!Form::close()!!}
-                    </div>
-                </div>
+                @include('frontend.box_contact')
                 @include('frontend.box_adv_center')
             </div><!--//col-left-->
             @include('frontend.right')
